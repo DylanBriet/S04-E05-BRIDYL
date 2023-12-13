ac09cd58bc374f07073007da1f8acfb8440ae6dc
BNoClock <benjamin.nougadere@oclock.io>
2023-12-13 09:58:17 +0100

    debut de correction

--- app/controllers/promoController.js
+++ app/controllers/promoController.js
@@ -6,6 +6,33 @@ const promoController = {
         res.locals.promoList = promoList;
 
         res.render("promos");
+    },
+    displayOne(req,res,next){
+
+
+        // req ? c'est la requête, Express enregistre dans l'objet "req" toutes les infos de la requête
+        // req.params ?
+        // req.params.michel ?
+        console.log(req.params);
+
+
+
+        // je récupère la promo par son id
+        const promo = promoList.find(promo => promo.id == req.params.michel);
+
+        // la promo existe-t-elle ?
+        if(promo){
+            res.locals.promo = promo;
+            res.render("promoDetails");
+        }
+        else{
+            // je renvoie la 404
+            // res.render("404");
+
+            // je passe au middleware suivant
+            next();
+        }
+        
     }
 };
 
--- app/router.js
+++ app/router.js
@@ -8,5 +8,15 @@ const router = Router();
 router.get("/",mainController.displayHomepage);
 
 router.get("/promos",promoController.displayAll);
+router.get("/promo/:michel",promoController.displayOne);
+
+// http://localhost:3000/promo/1/2/3/4
+// router.get("/promo/:toto/:tata/:tutu/:titi",promoController.displayOne);
+
+// Middleware de gestion de la 404
+// je ne gère l'envoi de la 404 qu'à un seul endroit
+router.use(()=>{
+    res.render("404");
+});
 
 module.exports = router;
